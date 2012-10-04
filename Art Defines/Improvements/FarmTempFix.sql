ed3a0685b1eeadd1ba82a70cde6a674a7c4333ba
Horem <horemvore@live.co.uk>
2012-10-04 18:33:21 +0100

    Resource Art Tweaks
    Tweaked Wild Berries and Flax. Added basic Polder art for wheat farm.

--- Art Defines/Improvements/FarmTempFix.sql
+++ Art Defines/Improvements/FarmTempFix.sql
@@ -1,24 +1,44 @@
 -- Create new Art for farm, Polder Graphic scaled down 20%
 -- Normal Farm
 INSERT INTO ArtDefine_Landmarks (Era,			State,					Scale,	ImprovementType,				LayoutHandler,	ResourceType,				Model,												TerrainContour, Tech)
-SELECT							 'Ancient',		'UnderConstruction',	0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'HB_ANC_Polder.fxsxml',								1,				null					UNION ALL
-SELECT							 'Ancient',		'UnderConstruction',	0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'ANC_Polder.fxsxml',								1,				null					UNION ALL
-SELECT							 'Ancient',		'Constructed',			0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'Polder_Blank.fxsxml',								1,				null					UNION ALL
-SELECT							 'Ancient',		'Constructed',			0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'ANC_Polder.fxsxml',								1,				null					UNION ALL
-SELECT							 'Ancient',		'Pillaged',				0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'PL_ANC_Polder.fxsxml',								1,				null					UNION ALL
-SELECT							 'Ancient',		'Pillaged',				0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'ANC_Polder.fxsxml',								1,				null					UNION ALL
-SELECT							 'Ancient',		'UnderConstruction',	0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'HB_IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
-SELECT							 'Ancient',		'UnderConstruction',	0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
-SELECT							 'Ancient',		'Constructed',			0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
-SELECT							 'Ancient',		'Constructed',			0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'Polder_Blank.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
-SELECT							 'Ancient',		'Pillaged',				0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'PL_IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
-SELECT							 'Ancient',		'Pillaged',				0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
-SELECT							 'Industrial',	'UnderConstruction',	0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'HB_IND_Polder.fxsxml',								1,				null					UNION ALL
-SELECT							 'Industrial',	'UnderConstruction',	0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				null					UNION ALL
-SELECT							 'Industrial',	'Constructed',			0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				null					UNION ALL
-SELECT							 'Industrial',	'Constructed',			0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'Polder_Blank.fxsxml',								1,				null					UNION ALL
-SELECT							 'Industrial',	'Pillaged',				0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'PL_IND_Polder.fxsxml',								1,				null					UNION ALL
-SELECT							 'Industrial',	'Pillaged',				0.8,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				null;
+SELECT							 'Ancient',		'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'HB_ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'Polder_Blank.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'PL_ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'HB_IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'Polder_Blank.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'PL_IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Industrial',	'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'HB_IND_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'Polder_Blank.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_NONE',	'PL_IND_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_NONE',	'IND_Polder.fxsxml',								1,				null;
+-- Wheat Farm
+INSERT INTO ArtDefine_Landmarks (Era,			State,					Scale,	ImprovementType,				LayoutHandler,	ResourceType,				Model,												TerrainContour, Tech)
+SELECT							 'Ancient',		'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_WHEAT',	'HB_ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_WHEAT',	'ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_WHEAT',	'Polder_Blank.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_WHEAT',	'ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_WHEAT',	'PL_ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_WHEAT',	'ANC_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Ancient',		'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_WHEAT',	'HB_IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_WHEAT',	'IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_WHEAT',	'IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_WHEAT',	'Polder_Blank.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_WHEAT',	'PL_IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Ancient',		'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_WHEAT',	'IND_Polder.fxsxml',								1,				'TECH_ECONOMICS'		UNION ALL
+SELECT							 'Industrial',	'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_WHEAT',	'HB_IND_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'UnderConstruction',	0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_WHEAT',	'IND_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_WHEAT',	'IND_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'Constructed',			0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_WHEAT',	'Polder_Blank.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'SNAPSHOT',		'ART_DEF_RESOURCE_WHEAT',	'PL_IND_Polder.fxsxml',								1,				null					UNION ALL
+SELECT							 'Industrial',	'Pillaged',				0.7,	'ART_DEF_IMPROVEMENT_NEWFARM',	'ANIMATED',		'ART_DEF_RESOURCE_WHEAT',	'IND_Polder.fxsxml',								1,				null;
 
 INSERT INTO ArtDefine_LandmarkTypes	(Type,							LandmarkType,	FriendlyName) 
 VALUES								('ART_DEF_IMPROVEMENT_NEWFARM',	'Improvement',	'Farm');
